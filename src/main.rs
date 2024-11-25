e72672144fd6a243beda85fb41bec325ea2036a0
Helios Bayma <heliosbayma@gmail.com>
2024-11-24 22:04:20 -0500

    change syntax - the previous was working fine but the analyzer was pointing errors - the actual syntax is accepted by the analyzer

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,26 @@ use tokio::net::TcpListener;
 const BIND_ADDRESS: &str = "127.0.0.1:80";
 const WWW_DIR: &str = "./www";
 
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-  fs::create_dir_all(WWW_DIR)?;
+fn main() -> Result<(), std::io::Error> {
+  // Create runtime
+  let runtime = tokio::runtime::Runtime::new()?;
 
-  let tcp_listener = TcpListener::bind(BIND_ADDRESS).await?;
-  println!("Server listening on port 80");
-  println!("Serving files from {} directory", WWW_DIR);
+  // Enter the runtime
+  runtime.block_on(async {
+    fs::create_dir_all(WWW_DIR)?;
 
-  loop {
-    let (socket, _) = tcp_listener.accept().await?;
+    let tcp_listener = TcpListener::bind(BIND_ADDRESS).await?;
+    println!("Server listening on port 80");
+    println!("Serving files from {} directory", WWW_DIR);
 
-    // Spawn a new task to handle the connection
-    tokio::spawn(async move {
-      if let Err(e) = server::handle_connection(socket).await {
-        eprintln!("Error handling connection: {}", e);
-      }
-    });
-  }
+    loop {
+      let (client_stream, _) = tcp_listener.accept().await?;
+
+      tokio::spawn(async move {
+        if let Err(e) = server::handle_connection(client_stream).await {
+          eprintln!("Error handling connection: {}", e);
+        }
+      });
+    }
+  })
 }
